1b00dd54cacfd3545525cb01687cc2e65e80f2e7
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2024-08-25 23:59:07 +0530

    Day 15 25th august problem solved

---
+++ 15_Problem145_BinaryTreePostorderTraversal.java
@@ -0,0 +1,63 @@
+/*
+
+
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        dfs(root.right);
+        ans.add(root.val);
+    }
+}
+
+/*
+Output:
+
+Case 1
+
+root = [1,null,2,3]
+1
+2
+3
+
+
+Case 2
+
+root = []
+
+
+Case 3
+
+root = [1]
+
+
+
+*/
